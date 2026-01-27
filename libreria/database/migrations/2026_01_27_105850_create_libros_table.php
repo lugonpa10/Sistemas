e86bb1d2b13aaf506da0d54c5a0973f019e185ec
lugonpa10 <lugonpa10@gmail.com>
2026-01-27 12:24:15 +0100

    changes

---
+++ libreria/database/migrations/2026_01_27_105850_create_libros_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table -> engine('InnoDB');
+
+            $table->id();
+            $table->bigInteger('categoria_id')->unsigned();
+            $table -> string('nombre');
+            $table->timestamps();
+            $table -> foreign('categoria_id') -> references('id')->on('categorias')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
