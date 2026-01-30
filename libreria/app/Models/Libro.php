753706bd097cfd2e256dc34ee4f542dc3c7f7f63
lugonpa10 <lugonpa10@gmail.com>
2026-01-30 12:26:41 +0100

    changes

---
+++ libreria/app/Models/Libro.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Libro
+ *
+ * @property $id
+ * @property $categoria_id
+ * @property $nombre
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Categoria $categoria
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Libro extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['categoria_id', 'nombre'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function categoria()
+    {
+        return $this->belongsTo(\App\Models\Categoria::class, 'categoria_id', 'id');
+    }
+    
+}
---
+++ libreria/resources/views/libro/form.blade.php
@@ -0,0 +1,19 @@
+<div class="row padding-1 p-1">
+    <div class="col-md-12">
+        
+        <div class="form-group mb-2 mb20">
+            <label for="categoria_id" class="form-label">{{ __('Categoria Id') }}</label>
+            <input type="text" name="categoria_id" class="form-control @error('categoria_id') is-invalid @enderror" value="{{ old('categoria_id', $libro?->categoria_id) }}" id="categoria_id" placeholder="Categoria Id">
+            {!! $errors->first('categoria_id', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+        <div class="form-group mb-2 mb20">
+            <label for="nombre" class="form-label">{{ __('Nombre') }}</label>
+            <input type="text" name="nombre" class="form-control @error('nombre') is-invalid @enderror" value="{{ old('nombre', $libro?->nombre) }}" id="nombre" placeholder="Nombre">
+            {!! $errors->first('nombre', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+
+    </div>
+    <div class="col-md-12 mt20 mt-2">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
